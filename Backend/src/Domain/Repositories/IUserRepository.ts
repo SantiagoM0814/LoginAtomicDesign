561c8a43c48b3df1faf8e364717facd81def3f79
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-22 11:07:11 -0500

    Feat: Obtiene todos los usuarios e importación de User

--- Backend/src/Domain/Repositories/IUserRepository.ts
+++ Backend/src/Domain/Repositories/IUserRepository.ts
@@ -1,6 +1,8 @@
+import { User } from "../Entities/User";
+
 export interface IUserRepository {
 
     save (user: User): Promise<User>;
     findByEmail (correo: string): Promise<User | null>;
-    
+    findAll(): Promise<User[]>;
 }
