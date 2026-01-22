9c0b889f8f9d58a8bd17707af98b785e10499b67
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-22 11:26:05 -0500

    Feat: Importaciones de otros archivos añadidos

--- Backend/src/Application/Use-Cases/RegisterUser.ts
+++ Backend/src/Application/Use-Cases/RegisterUser.ts
@@ -1,3 +1,8 @@
+import { IUserRepository } from "../../Domain/Repositories/IUserRepository";
+import { AuthService } from "../Services/AuthService";
+import { RegisterDTO } from "../DTOs/RegisterDTO";
+import { UserDTO } from "../DTOs/UserDTO";
+
 export class RegisterUserUseCase {
 
     constructor (
@@ -6,5 +11,12 @@ export class RegisterUserUseCase {
         private authService: AuthService
     ) {}
 
-    
+    async execute (data: RegisterDTO): Promise<UserDTO> {
+
+        const { correo, contrasena, nombre } = data;
+
+        if (!this.authService.validateEmail(correo)) {
+            throw new Error('Invalid email format');
+        }
+    }
 }
