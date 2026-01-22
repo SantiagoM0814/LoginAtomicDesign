88f639677b562d35270a41bac619899bce4da44e
MauricioCordoba-Dev <carlosmauricioelcrack@gmail.com>
2026-01-22 16:33:27 -0500

    feat: se crea RegisterPage y se agregan enlaces de navegación

---
+++ atomic-design-front/src/components/pages/Register/RegisterPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import RegisterForm from '../../organisms/RegisterForm/RegisterForm';
+import './RegisterPage.css';
+import './RegisterPageCustom.css';
+
+const RegisterPage: React.FC = () => {
+  const handleRegister = (name: string, email: string, password: string) => {
+
+    console.log('Registro:', name, email, password);
+  };
+
+  return (
+    <div className="register-page-container">
+      <div className="register-page-box">
+        <h1 className="register-title">Registro</h1>
+        <RegisterForm onSubmit={handleRegister} />
+        <p className="register-login-link">
+          ¿Ya tienes cuenta?{' '}
+          <Link to="/">Inicia sesión aquí</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
