50b3b186e79d40260cf9ddbe6a4168beb5903215
Asasing37 <samuelcuidaesquivel@gmail.com>
2026-01-23 12:53:01 -0500

    Feat: Mock del repositorio de los ususarios creado

--- Backend/src/Application/UseCases/GetUser.test.ts
+++ Backend/src/Application/UseCases/GetUser.test.ts
@@ -1,5 +1,18 @@
+import { GetUsers } from '../UseCases/GetUsers';
+import { IUserRepository } from '../../Domain/Repositories/IUserRepository';
+
 describe ('GetUsers', () => {
 
     let getUsersUseCase: GetUsers;
     let mockUserRepository: jest.Mocked<IUserRepository>;
+
+    beforeEach(() => {
+
+        mockUserRepository = {
+
+            findAll: jest.fn(),
+            findByEmail: jest.fn(),
+            save: jest.fn()
+        };
+    })
 })
